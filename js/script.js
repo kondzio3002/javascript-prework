3a8882fdf4e359b78deac1cd1ebc7c5ae31ff217
Konrad Brożyna <kondzio3002@gmail.com>
2022-08-07 15:12:06 +0200

    Add variables for js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Variables
+
+let computerMove='kamień';
+printMessage('Zagrałem '+computerMove+'! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier' ;
+printMessage('Zagrałem ' + playerMove + " ! WYRGYWAM !");
