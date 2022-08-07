dec2fa0511c8962a81d3fdf203d4048e33c3597e
Konrad Brożyna <kondzio3002@gmail.com>
2022-08-07 16:14:27 +0200

    Add computer move

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-//Variables
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove='kamień';
-printMessage('Zagrałem '+computerMove+'! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'papier' ;
-printMessage('Zagrałem ' + playerMove + " ! WYRGYWAM !");
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2){
+    computerMove = 'papier';
+} else {
+    computerMove = 'nożyce';
+}
